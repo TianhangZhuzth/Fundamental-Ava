7c241a2ba276b3e69f5285357a677d9ca1b987cb
TianhangZhuzth <tianhangzhuzth@users.noreply.github.com>
2026-05-03 12:22:12 +0000

    feat: scaffold __init__.py

---
+++ src/ava/communication/__init__.py
@@ -0,0 +1,4 @@
+from ava.communication.consensus import ConsensusError, RaftLikeConsensus
+from ava.communication.protocol import Message, MessageBus, MessageType
+
+__all__ = ["ConsensusError", "Message", "MessageBus", "MessageType", "RaftLikeConsensus"]
